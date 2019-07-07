9564fdf6701e1adb8f6c167f63c82f384d7fa244
dsying <dingduoduo0916@gmail.com>
2019-07-08 00:19:23 +0800

    子类会继承父类的 成员变量和方法 (#18)
    子类会继承父类的 成员变量和方法 (#18)

---
+++ src/main/java/com/github/hcsp/inheritance/Animal.java
@@ -0,0 +1,13 @@
+package com.github.hcsp.inheritance;
+
+public class Animal {
+  protected String name;
+
+  protected Animal(String name) {
+    this.name = name;
+  }
+
+  protected void sayMyName() {
+    System.out.println("我的名字是" + name);
+  }
+}
