710712a9d885638d4969adc6a228062abc17b829
liboyang <huagongyilby@126.com>
2019-07-17 16:30:08 +0800

    继承

--- src/main/java/com/github/hcsp/inheritance/Cat.java
+++ src/main/java/com/github/hcsp/inheritance/Cat.java
@@ -1,14 +1,10 @@
 package com.github.hcsp.inheritance;
 
-public class Cat {
-    private String name;
+public class Cat extends Animal{
 
     public Cat(String name) {
+        super(name);
         this.name = name;
-    }
-
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
     }
 
     public void meow() {
--- src/main/java/com/github/hcsp/inheritance/Rat.java
+++ src/main/java/com/github/hcsp/inheritance/Rat.java
@@ -1,14 +1,10 @@
 package com.github.hcsp.inheritance;
 
-public class Rat {
-    private String name;
+public class Rat extends Animal{
 
     public Rat(String name) {
+        super(name);
         this.name = name;
-    }
-
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
     }
 
     public void zhizhi() {
