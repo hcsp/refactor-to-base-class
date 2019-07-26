5ea0cdaa8dbb8814cec29df0967d1648c7753d80
Ben <50296011+MaoYuanFang@users.noreply.github.com>
2019-07-26 18:31:47 +0800

    1 (#31)
    1 (#31)

--- src/main/java/com/github/hcsp/inheritance/Rat.java
+++ src/main/java/com/github/hcsp/inheritance/Rat.java
@@ -1,17 +1,12 @@
 package com.github.hcsp.inheritance;
 
-public class Rat {
-    private String name;
+public class Rat extends Animals{
 
     public Rat(String name) {
-        this.name = name;
+        super(name);
     }
 
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
-    }
-
-    public void zhizhi() {
+    void zhizhi() {
         System.out.println("吱吱" + name);
     }
 }
