52a07ee0c46f3aa0f16445e361a1625e150b836d
tangfei <497045233@qq.com>
2019-08-10 23:01:15 +0800

    将公用代码抽取到父类中

--- src/main/java/com/github/hcsp/inheritance/Animal.java
+++ src/main/java/com/github/hcsp/inheritance/Animal.java
@@ -1,7 +1,7 @@
 package com.github.hcsp.inheritance;
 
 public class Animal {
-    public String name;
+     String name;
 
     public void sayMyName() {
         System.out.println("我的名字是" + name);
