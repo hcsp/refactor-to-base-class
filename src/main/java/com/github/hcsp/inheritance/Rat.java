69668823f945c1182f646cdf93f3cf3ed3fc4155
zhuangsuyuHP <zhuangsuyu1993@163.com>
2019-08-20 13:04:25 +0800

    将公用代码抽取到父类中

--- src/main/java/com/github/hcsp/inheritance/Rat.java
+++ src/main/java/com/github/hcsp/inheritance/Rat.java
@@ -1,14 +1,9 @@
 package com.github.hcsp.inheritance;
 
-public class Rat {
-    private String name;
+public class Rat extends AbstractAnimal {
 
     public Rat(String name) {
-        this.name = name;
-    }
-
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
+        super(name);
     }
 
     public void zhizhi() {
