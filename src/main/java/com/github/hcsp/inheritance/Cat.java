648b004041d5667fe83d75c4e19ee7e5ef9c9d7f
qiaomengnan <2568151682@qq.com>
2019-10-02 08:47:00 +0800

    base class

--- src/main/java/com/github/hcsp/inheritance/Cat.java
+++ src/main/java/com/github/hcsp/inheritance/Cat.java
@@ -1,14 +1,9 @@
 package com.github.hcsp.inheritance;
 
-public class Cat {
-    private String name;
+public class Cat extends Animal {
 
     public Cat(String name) {
         this.name = name;
-    }
-
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
     }
 
     public void meow() {
