04c2742e80e30a052ffaca7a0b7b53ad64eba0c1
xuanzebin <38971117+xuanzebin@users.noreply.github.com>
2019-10-13 11:39:44 +0800

    feat: refactor to base class (#70)
    feat: refactor to base class (#70)

---
+++ src/main/java/com/github/hcsp/inheritance/Animal.java
@@ -0,0 +1,11 @@
+package com.github.hcsp.inheritance;
+
+class Animal {
+    String name;
+    Animal(String name) {
+        this.name = name;
+    }
+    void sayMyName() {
+        System.out.println("我的名字是" + this.name);
+    }
+}
