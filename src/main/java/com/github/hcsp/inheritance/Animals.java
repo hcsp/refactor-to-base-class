254dd6c2ffd1d105eacf24a3df50134968986ae1
TangsongyuanmingQingHAD <had13625428701@aliyun.com>
2019-12-11 14:37:17 +0800

    继承

---
+++ src/main/java/com/github/hcsp/inheritance/Animals.java
@@ -0,0 +1,12 @@
+package com.github.hcsp.inheritance;
+
+public class Animals {
+    private String name;
+    public Animals(String name) {
+        this.name = name;
+    }
+
+    public void sayMyName() {
+        System.out.println("我的名字是" + name);
+    }
+}
