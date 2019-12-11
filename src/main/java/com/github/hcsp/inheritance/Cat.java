a67d7ba48af54c9404f598f821ea6b98ce0a7658
TangsongyuanmingQingHAD <had13625428701@aliyun.com>
2019-12-11 14:36:53 +0800

    继承

--- src/main/java/com/github/hcsp/inheritance/Cat.java
+++ src/main/java/com/github/hcsp/inheritance/Cat.java
@@ -1,15 +1,13 @@
 package com.github.hcsp.inheritance;
 
-public class Cat {
+public class Cat extends  Animals{
     private String name;
 
     public Cat(String name) {
+        super(name);
         this.name = name;
     }
 
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
-    }
 
     public void meow() {
         System.out.println("喵" + name);
