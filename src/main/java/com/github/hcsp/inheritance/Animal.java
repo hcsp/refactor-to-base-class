f4e7be18710be1109ecd88826b96440eff3b55df
JaneWangCoA <43361638+JaneWangCOA@users.noreply.github.com>
2019-12-24 14:48:00 +0800

    继承 (#101)
    继承 (#101)

---
+++ src/main/java/com/github/hcsp/inheritance/Animal.java
@@ -0,0 +1,13 @@
+package com.github.hcsp.inheritance;
+
+public class Animal {
+    String name;
+
+    Animal(String name) {
+        this.name = name;
+    }
+
+    void sayMyName() {
+        System.out.println("我的名字是" + name);
+    }
+}
--- src/main/java/com/github/hcsp/inheritance/Rat.java
+++ src/main/java/com/github/hcsp/inheritance/Rat.java
@@ -1,17 +1,13 @@
 package com.github.hcsp.inheritance;
 
-public class Rat {
-    private String name;
-
-    public Rat(String name) {
-        this.name = name;
-    }
-
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
-    }
-
+public class Rat extends Animal {
     public void zhizhi() {
         System.out.println("吱吱" + name);
     }
+
+    public Rat(String name) {
+        super(name);
+    }
+
+
 }
