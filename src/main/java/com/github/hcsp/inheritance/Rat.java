ddafa07a84fc33a852829df0d314a831b5c34310
xdcgh <44773625+xdcgh@users.noreply.github.com>
2020-01-01 20:20:33 +0800

    初步完成：将公用代码抽取到父类中 (#106)
    初步完成：将公用代码抽取到父类中 (#106)

--- src/main/java/com/github/hcsp/inheritance/Rat.java
+++ src/main/java/com/github/hcsp/inheritance/Rat.java
@@ -1,17 +1,11 @@
 package com.github.hcsp.inheritance;
 
-public class Rat {
-    private String name;
-
+public class Rat extends Animal{
     public Rat(String name) {
-        this.name = name;
-    }
-
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
+        super(name);
     }
 
     public void zhizhi() {
-        System.out.println("吱吱" + name);
+        System.out.println("吱吱" + this.getName());
     }
 }
