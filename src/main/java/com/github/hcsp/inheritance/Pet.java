c8dcbe773794b158d53468a64ae4af3bbe4365f9
Leo Feng <553931681@qq.com>
2020-02-24 14:04:35 +0800

    将公用代码抽取到父类 (#132)
    将公用代码抽取到父类 (#132)

---
+++ src/main/java/com/github/hcsp/inheritance/Pet.java
@@ -0,0 +1,14 @@
+package com.github.hcsp.inheritance;
+
+public class Pet {
+
+    protected String name;
+
+    public Pet(String name) {
+        this.name = name;
+    }
+
+    public void sayMyName() {
+        System.out.println("我的名字是" + name);
+    }
+}
