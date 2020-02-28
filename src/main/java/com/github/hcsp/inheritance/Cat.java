714ae14253e188722fdbcb1238398cc43da6d1af
pathjh <1639925487@qq.com>
2020-02-28 19:13:59 +0800

    继承

--- src/main/java/com/github/hcsp/inheritance/Cat.java
+++ src/main/java/com/github/hcsp/inheritance/Cat.java
@@ -1,11 +1,10 @@
 package com.github.hcsp.inheritance;
 
 public class Cat extends Animal {
-    private String name;
+
 
     public Cat(String name) {
         super(name);
-        this.name = name;
     }
 
     public void meow() {
--- src/main/java/com/github/hcsp/inheritance/Dog.java
+++ src/main/java/com/github/hcsp/inheritance/Dog.java
@@ -1,11 +1,9 @@
 package com.github.hcsp.inheritance;
 
 public class Dog extends Animal {
-    private String name;
 
     public Dog(String name) {
         super(name);
-        this.name = name;
     }
 
     public void wang() {
