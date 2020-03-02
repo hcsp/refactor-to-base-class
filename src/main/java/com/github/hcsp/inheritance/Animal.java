1073cefd200c7f25a9137cd68fdc045d5d1a7a0d
sun1341283 <53633837+sun1341283@users.noreply.github.com>
2020-03-02 22:04:31 +0800

    继承 (#139)
    继承 (#139)

---
+++ src/main/java/com/github/hcsp/inheritance/Animal.java
@@ -0,0 +1,16 @@
+package com.github.hcsp.inheritance;
+
+public  class Animal {
+     String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public Animal() {
+    }
+
+    public void sayMyName(){
+        System.out.println("我的名字是" + name);
+    }
+}
