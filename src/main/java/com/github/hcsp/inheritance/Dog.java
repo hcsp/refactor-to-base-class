23c7bc1d48f77787f696c5c33cead63f403c5ac8
DobbyKim <1175088275@qq.com>
2020-03-04 20:02:31 +0800

    add code

--- src/main/java/com/github/hcsp/inheritance/Dog.java
+++ src/main/java/com/github/hcsp/inheritance/Dog.java
@@ -1,17 +1,14 @@
 package com.github.hcsp.inheritance;
 
-public class Dog {
+public class Dog extends Animal{
     private String name;
 
-    public Dog(String name) {
+    public Dog(String name){
+        super(name);
         this.name = name;
     }
 
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
-    }
-
     public void wang() {
-        System.out.println("汪" + name);
+        System.out.println("汪" + this.name);
     }
 }
