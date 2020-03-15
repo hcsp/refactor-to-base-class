1f9407f515307d7626079a9a0d3497266ce1a4fd
arsonalXxu <897386402@qq.com>
2020-03-15 18:07:58 +0800

    将公用代码抽取到父类；

---
+++ src/main/java/com/github/hcsp/inheritance/Animal.java
@@ -0,0 +1,17 @@
+package com.github.hcsp.inheritance;
+
+public class Animal {
+    public String name;
+
+    public Animal() {
+
+    }
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public void sayMyName() {
+        System.out.println("我的名字是" + name);
+    }
+}
