42b9c5331c6ed92516e0a25a514735674e15cfae
XinleZhou <2320789321>
2020-03-21 15:15:12 +0800

    jicheng

--- src/main/java/com/github/hcsp/inheritance/Rat.java
+++ src/main/java/com/github/hcsp/inheritance/Rat.java
@@ -1,15 +1,12 @@
 package com.github.hcsp.inheritance;
 
-public class Rat {
+public class Rat extends Animal{
     private String name;
 
-    public Rat(String name) {
-        this.name = name;
+    Rat(String name) {
+        super(name);
     }
 
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
-    }
 
     public void zhizhi() {
         System.out.println("吱吱" + name);
