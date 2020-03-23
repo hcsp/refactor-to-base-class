5e0eb900578d51973903a235eb6877c0f79d1f04
gofocus <go_focus@live.cn>
2020-03-23 14:27:49 +0800

    将公用代码抽取到父类中

---
+++ src/main/java/com/github/hcsp/inheritance/Animal.java
@@ -0,0 +1,13 @@
+package com.github.hcsp.inheritance;
+
+/**
+ * @Author: GoFocus
+ * @Date: 2020-03-23 14:23
+ * @Description:
+ */
+public class Animal {
+    protected String name;
+    public void sayMyName() {
+        System.out.println("我的名字是" + name);
+    }
+}
