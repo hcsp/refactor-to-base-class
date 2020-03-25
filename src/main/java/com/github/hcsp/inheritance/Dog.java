2575b49d73c44ddbc09c59d2b5b590f3e86b01c1
Enoch Qin <qyqdream@gmail.com>
2020-03-26 00:07:08 +0800

    继承 (#157)
    继承 (#157)

--- src/main/java/com/github/hcsp/inheritance/Dog.java
+++ src/main/java/com/github/hcsp/inheritance/Dog.java
@@ -1,17 +1,11 @@
 package com.github.hcsp.inheritance;
 
-public class Dog {
-    private String name;
-
-    public Dog(String name) {
-        this.name = name;
+public class Dog extends Animal {
+    Dog(String name) {
+        super(name);
     }
 
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
-    }
-
-    public void wang() {
+    void wang() {
         System.out.println("汪" + name);
     }
 }
--- src/main/java/com/github/hcsp/inheritance/Rat.java
+++ src/main/java/com/github/hcsp/inheritance/Rat.java
@@ -1,17 +1,11 @@
 package com.github.hcsp.inheritance;
 
-public class Rat {
-    private String name;
-
-    public Rat(String name) {
-        this.name = name;
+public class Rat extends Animal {
+    Rat(String name) {
+        super(name);
     }
 
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
-    }
-
-    public void zhizhi() {
+    void zhizhi() {
         System.out.println("吱吱" + name);
     }
 }
