1e3c5d09e42d60105d0310e88f6cf3979d059aaa
Lance <491799643@qq.com>
2020-04-19 17:38:44 +0800

    抽取共性代码形成父类

--- src/main/java/com/github/hcsp/inheritance/Cat.java
+++ src/main/java/com/github/hcsp/inheritance/Cat.java
@@ -1,17 +1,13 @@
 package com.github.hcsp.inheritance;
 
-public class Cat {
+public class Cat extends Animal {
     private String name;
 
     public Cat(String name) {
-        this.name = name;
-    }
-
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
+        super(name);
     }
 
     public void meow() {
-        System.out.println("喵" + name);
+        System.out.println("喵" + super.getName());
     }
 }
