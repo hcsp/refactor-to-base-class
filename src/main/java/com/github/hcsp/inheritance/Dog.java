ae458d0e65ee273578acb7f75e87f134432551e3
lionlihuang <65241440+lionlihuang@users.noreply.github.com>
2020-05-17 16:01:45 +0800

    将公用代码抽取到父类中 (#181)
    将公用代码抽取到父类中 (#181)

--- src/main/java/com/github/hcsp/inheritance/Dog.java
+++ src/main/java/com/github/hcsp/inheritance/Dog.java
@@ -1,17 +1,17 @@
 package com.github.hcsp.inheritance;
 
-public class Dog {
-    private String name;
+public class Dog extends Animal {
+
 
     public Dog(String name) {
-        this.name = name;
+
+        super(name);
     }
 
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
-    }
+
 
     public void wang() {
+
         System.out.println("汪" + name);
     }
 }
