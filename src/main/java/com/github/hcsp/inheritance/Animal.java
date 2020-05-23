5f079aa435e1e61238ea9e9c73f9611fea4763a8
hanchenhao <hanchenhao@vip.qq.com>
2020-05-24 00:14:04 +0800

    base class

--- src/main/java/com/github/hcsp/inheritance/Animal.java
+++ src/main/java/com/github/hcsp/inheritance/Animal.java
@@ -1,11 +1,19 @@
 package com.github.hcsp.inheritance;
 
 public class Animal {
-    public String name;
+    private String name;
     public Animal(String name) {
         this.name = name;
     }
     public void sayMyName() {
         System.out.println("我的名字是" + name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
