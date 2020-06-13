43586d221db35f1cd2e062f410525b6c9bc82157
Leo <790868516@qq.com>
2020-06-13 19:10:28 +0800

    将公用代码抽取到父类中

--- src/main/java/com/github/hcsp/inheritance/Cat.java
+++ src/main/java/com/github/hcsp/inheritance/Cat.java
@@ -1,15 +1,11 @@
 package com.github.hcsp.inheritance;
 
-public class Cat {
-    private String name;
+public class Cat extends Animals {
 
     public Cat(String name) {
         this.name = name;
     }
 
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
-    }
 
     public void meow() {
         System.out.println("喵" + name);
