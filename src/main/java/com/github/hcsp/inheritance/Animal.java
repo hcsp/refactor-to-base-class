48976752c77fd2ee8875c54c4346040d02e4b77e
lr6 <36.zhibo@gmail.com>
2020-08-17 23:36:17 +0800

    抽象父类

---
+++ src/main/java/com/github/hcsp/inheritance/Animal.java
@@ -0,0 +1,17 @@
+package com.github.hcsp.inheritance;
+
+public class Animal {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void sayMyName() {
+        System.out.println("我的名字是" + name);
+    }
+
+    public Animal (String name) {
+        this.name = name;
+    }
+}
