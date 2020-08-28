ede5eb34a61c2304a3a8a98beac4a54f1ba8a73e
pccmast <35651458+pccmast@users.noreply.github.com>
2020-08-28 15:59:11 +0800

    fix (#201)
    fix (#201)

--- src/main/java/com/github/hcsp/inheritance/Cat.java
+++ src/main/java/com/github/hcsp/inheritance/Cat.java
@@ -1,17 +1,11 @@
 package com.github.hcsp.inheritance;
 
-public class Cat {
-    private String name;
-
+public class Cat extends Animal{
     public Cat(String name) {
-        this.name = name;
-    }
-
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
+        super(name);
     }
 
     public void meow() {
-        System.out.println("喵" + name);
+        System.out.println("喵" + this.getName());
     }
 }
