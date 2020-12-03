ac3e48eb051ee1dda05f8048c7f16602a9ab7433
weiranyi <168578203@qq.com>
2020-12-03 18:06:04 +0800

    创建Animal类

---
+++ src/main/java/com/github/hcsp/inheritance/Animal.java
@@ -0,0 +1,17 @@
+package com.github.hcsp.inheritance;
+
+public class Animal {
+    //声明一个存储名字的字符串变量
+    private String name;
+
+    //修改名字的方法
+    public Animal(String name) {
+        this.name = name;
+    }
+
+
+    public void sayMyName() {
+        System.out.println("我的名字是" + name);
+    }
+
+}
