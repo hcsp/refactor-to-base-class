a2835228ce76e710dddafd3527e518df63a31d47
zyf <3440422354@qq.com>
2021-03-16 12:57:12 +0800

    zyf

---
+++ src/main/java/com/github/hcsp/inheritance/Animal.java
@@ -0,0 +1,17 @@
+package com.github.hcsp.inheritance;
+
+public class Animal {
+    public String getName() {
+        return name;
+    }
+
+    private String name;
+    public Animal(String name) {
+        this.name = name;
+    }
+    public void sayMyName() {
+        System.out.println("我的名字是" + name);
+    }
+
+
+}
