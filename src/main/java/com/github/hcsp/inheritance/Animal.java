1d5f7785a991773301e5cd341c57cdd6cc2409cf
AdFeng <3200185362@qq.com>
2021-10-08 18:13:02 +0800

    继承练习

---
+++ src/main/java/com/github/hcsp/inheritance/Animal.java
@@ -0,0 +1,16 @@
+package com.github.hcsp.inheritance;
+
+public class Animal {
+    String name;
+
+    public Animal() {
+    }
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public void sayMyName() {
+        System.out.println("我的名字是" + name);
+    }
+}
