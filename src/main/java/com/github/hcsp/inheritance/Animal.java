d6e2578b0680f2d5c0980991068a89051c7e461f
饶家俊 <471412450@qq.com>
2021-10-10 00:46:53 +0800

    封装

--- src/main/java/com/github/hcsp/inheritance/Animal.java
+++ src/main/java/com/github/hcsp/inheritance/Animal.java
@@ -1,24 +1,18 @@
 package com.github.hcsp.inheritance;
 
 public class Animal {
+    public String getName() {
+        return name;
+    }
 
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  private String name;
-
-  public Animal(String name) {
-    this.name = name;
-  }
+    public Animal(String name) {
+        this.name = name;
+    }
 
 
-  public void sayMyName() {
-    System.out.println("我的名字是" + this.name);
-  }
+    private final String name;
+    public void sayMyName() {
+        System.out.println("我的名字是" + name);
+    }
 
 }
--- src/main/java/com/github/hcsp/inheritance/Rat.java
+++ src/main/java/com/github/hcsp/inheritance/Rat.java
@@ -1,10 +1,18 @@
 package com.github.hcsp.inheritance;
 
 public class Rat extends Animal{
-
     public Rat(String name) {
         super(name);
     }
+//    private String name;
+
+//    public Rat(String name) {
+//        this.name = name;
+//    }
+
+//    public void sayMyName() {
+//        System.out.println("我的名字是" + name);
+//    }
 
     public void zhizhi() {
         System.out.println("吱吱" + super.getName());
