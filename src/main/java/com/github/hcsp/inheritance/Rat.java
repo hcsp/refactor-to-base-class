f30e31419bb8bf718e2ee881339c266037a4f3a0
tq13111 <63287795+tq13111@users.noreply.github.com>
2021-11-11 08:53:11 +0800

    将公用代码抽取到父类中 (#246)
    将公用代码抽取到父类中 (#246)

--- src/main/java/com/github/hcsp/inheritance/Rat.java
+++ src/main/java/com/github/hcsp/inheritance/Rat.java
@@ -1,14 +1,10 @@
 package com.github.hcsp.inheritance;
 
-public class Rat {
-    private String name;
+public class Rat extends Animal{
+
 
     public Rat(String name) {
-        this.name = name;
-    }
-
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
+        super(name);
     }
 
     public void zhizhi() {
