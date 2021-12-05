15c96f90ce15de40f98833e8f63898bc74a66f35
lee <lindamoxiaobei@163.com>
2021-12-05 18:56:07 +0000

    constructor extensions from parent class.

--- src/main/java/com/github/hcsp/inheritance/Cat.java
+++ src/main/java/com/github/hcsp/inheritance/Cat.java
@@ -1,14 +1,12 @@
 package com.github.hcsp.inheritance;
 
-public class Cat {
+public class Cat extends Animal{
+
     private String name;
 
     public Cat(String name) {
+        super(name);
         this.name = name;
-    }
-
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
     }
 
     public void meow() {
