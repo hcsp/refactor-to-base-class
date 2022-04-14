6db44a011e5b73cfcfd4e28dd862ee9bf626bb5d
RyanLin514 <101817394+RyanLin514@users.noreply.github.com>
2022-04-14 17:26:18 +0800

    我已完成将公用代码抽取到父类中的作业 (#269)
    我已完成将公用代码抽取到父类中的作业 (#269)

--- src/main/java/com/github/hcsp/inheritance/Rat.java
+++ src/main/java/com/github/hcsp/inheritance/Rat.java
@@ -1,14 +1,9 @@
 package com.github.hcsp.inheritance;
 
-public class Rat {
-    private String name;
+public class Rat extends Animal{
 
     public Rat(String name) {
         this.name = name;
-    }
-
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
     }
 
     public void zhizhi() {
