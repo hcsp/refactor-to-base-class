90cef1aff65aa96b781953d7b2495c3b6aefeaae
zj <woaiwrf@qq.com>
2022-06-21 11:11:54 +0800

    继承

---
+++ src/main/java/com/github/hcsp/inheritance/Animal.java
@@ -0,0 +1,15 @@
+package com.github.hcsp.inheritance;
+
+public class Animal {
+    String name;
+    public void sayMyName(){
+        System.out.println("我的名字是" + name);
+    }
+
+    public Animal() {
+    }
+
+    public Animal(String name) {
+        this.name = name;
+    }
+}
